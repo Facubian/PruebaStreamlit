a8abd893d1f23792e084763027458aa3707ef1c5
Facubian <102619058+Facubian@users.noreply.github.com>
2024-08-30 09:52:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def primer_arbol():
       "amh": amh,
       "total rfa": rfa}
     
-    variables = pd.DataFrame([[edad,amh,rfa]])
+    variables = pd.DataFrame([variables])
     
     pred = modelo(variables,ovr)
 
