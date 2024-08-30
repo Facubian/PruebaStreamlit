1d81424180fff134be63ba0716d4877a6557b4c1
Facubian <102619058+Facubian@users.noreply.github.com>
2024-08-30 09:47:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,10 @@ def primer_arbol():
     
     pred = modelo(variables,ovr)
 
-    print(pred)
-    print(type(pred))
+    p = type(pred)
+
+    st.write(p)
+      
     if(pred==1):
       st.markdown("Con un **67%** de probabilidad, se esperan obtener entre **0 y 4**")
     elif(pred==2):
